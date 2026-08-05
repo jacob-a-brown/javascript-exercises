73752a55b95eab856db0234446d4886c29641b14
Jacob Brown <brown.jacob.alexander@gmail.com>
2026-08-05 09:30:51 -0600

    07_reverseString solution

--- foundations/07_reverseString/reverseString.js
+++ foundations/07_reverseString/reverseString.js
@@ -1,5 +1,8 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+    const strArray = str.split("");
+    strArray.reverse();
+    const reversedStr = strArray.join("")
+    return reversedStr;
 };
 
 // Do not edit below this line
