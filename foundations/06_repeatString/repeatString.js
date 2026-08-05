a8fd16dcb9e1119acbe2320b71841cd458d9290e
Jacob Brown <brown.jacob.alexander@gmail.com>
2026-08-05 09:17:57 -0600

    06_repeatString solution

--- foundations/06_repeatString/repeatString.js
+++ foundations/06_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
-
+const repeatString = function(str, n) {
+    if (n < 0) {
+        return "ERROR";
+    }
+    let arr = [];
+    for (i = 0; i < n; i++) {
+        arr.push(str);
+    }
+    const repeatedStr = arr.join("");
+    return repeatedStr;
 };
 
 // Do not edit below this line
