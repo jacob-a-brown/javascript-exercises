c4701b76a574f49835f27fe87618335029eed759
Jacob Brown <brown.jacob.alexander@gmail.com>
2026-08-05 09:57:43 -0600

    09_sumAll solution

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,5 +1,27 @@
-const sumAll = function() {
+const sumAll = function(a, b) {
+    if (!Number.isInteger(a) || !Number.isInteger(b)){
+        return "ERROR";
+    } else if (a < 0 || b < 0) {
+        return "ERROR";
+    }
+    
+    let start;
+    let end;
 
+    if (a < b) {
+        start = a;
+        end = b;
+    } else {
+        start = b;
+        end = a;
+    }
+    
+
+    let sum = 0;
+    for (let i = start; i <= end; i++){
+        sum += i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
