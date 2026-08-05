d3b8c37a404262b770c255105a511400c73c183a
Jacob Brown <brown.jacob.alexander@gmail.com>
2026-08-05 10:19:01 -0600

    11_tempConversion solution

--- foundations/11_tempConversion/tempConversion.js
+++ foundations/11_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temp) {
+  let celcius = (temp-32) * (5/9);
+  let celciusRounded = Math.round(celcius * 10) / 10;
+  return celciusRounded;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temp) {
+  let fahrenheit = (temp * (9/5)) + 32;
+  let fahrenheitRounded = Math.round(fahrenheit * 10) / 10;
+  return fahrenheitRounded;
 };
 
 // Do not edit below this line
