ef56241e2438a3dc377c237eef9686b3ffefd1a9
Jacob Brown <brown.jacob.alexander@gmail.com>
2026-08-05 09:39:06 -0600

    08_removeFromArray solution

--- foundations/08_removeFromArray/removeFromArray.js
+++ foundations/08_removeFromArray/removeFromArray.js
@@ -1,4 +1,9 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...itemsToRemove) {
+    // itemsToRemove is an array of the items to remove from the original array
+    // if an item is not in itemsToRemove keep it in the original array
+    // that is, it should evaluate to "true" for the filter callback function
+    const filteredArr = arr.filter(item => !itemsToRemove.includes(item));
+    return filteredArr;
 };
 
 // Do not edit below this line
