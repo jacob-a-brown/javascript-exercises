10ef34b4c4b8285203750b58ef4392a45670cef3
Jacob Brown <brown.jacob.alexander@gmail.com>
2026-08-07 14:22:49 -0600

    solution to 12_calculator

--- foundations/12_calculator/calculator.js
+++ foundations/12_calculator/calculator.js
@@ -1,25 +1,35 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a+b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a-b;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	let total = arr.reduce(function (sum, num) {
+    return sum += num;
+  }, 0);
+  return total;
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  let total = arr.reduce (function (product, num) {
+    return product *= num;
+  }, 1);
+  return total;
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return a**b;
 };
 
-const factorial = function() {
-	
+const factorial = function(n) {
+  let f = 1;
+  for (let i = 1; i <= n; i++){
+    f *= i;
+  }
+  return f;
 };
 
 // Do not edit below this line
